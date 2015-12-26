c1a6971e73b59a152880b3efd5cc895c0965ba22
Moinul Islam <moinulmithu@hotmail.com>
2015-12-26 22:55:02 +0600

    Full complete of topic one of the project

--- TaskManagement.Web/scripts/dashboard/dashboard.controller.js
+++ TaskManagement.Web/scripts/dashboard/dashboard.controller.js
@@ -1,4 +1,14 @@
 ﻿angular.module('tasklist').controller('dashboardController', ['$scope', function ($scope) {
     $scope.pagename = "Dashboard";
+    $scope.projects = ['Family', 'Work'];
+        $scope.selectedProject = '';
+    $scope.tasks = [];
+    $scope.loadTasks = function (p) {
+        $scope.selectedProject = p;
+        $scope.tasks = [];
+        for (var i = 0; i < 5; i++) {
+            $scope.tasks.push(p + i);
+        }
+    }
     }
 ]);
