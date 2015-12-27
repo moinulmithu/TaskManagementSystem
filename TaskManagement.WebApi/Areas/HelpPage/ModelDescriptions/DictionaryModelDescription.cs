0ea38747688393ab4fadf81015bdc8f88bfb170f
Moinul Islam <moinulmithu@hotmail.com>
2015-12-27 22:03:32 +0600

    Three new project added

---
+++ TaskManagement.WebApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace TaskManagement.WebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ TaskManagement.WebApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TaskManagement.WebApi.WebApiApplication" Language="C#" %>
