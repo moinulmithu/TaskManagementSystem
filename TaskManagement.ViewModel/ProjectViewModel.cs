bfd4dbd233c01e74055799962cef615f62f176ba
Moinul Islam <moinulmithu@hotmail.com>
2015-12-27 23:53:48 +0600

    One new project is added

---
+++ TaskManagement.ViewModel/ProjectViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagement.ViewModel
+{
+    public class ProjectViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Count { get; set; }
+    }
+}
---
+++ TaskManagement.WebApi/Controllers/ProjectController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TaskManagement.ViewModel;
+
+namespace TaskManagement.WebApi.Controllers
+{
+    public class ProjectController : ApiController
+    {
+        public List<ProjectViewModel> Get()
+        {
+            return new List<ProjectViewModel>() {new ProjectViewModel() {Id = 1, Name = "Test", Count = 0}};
+        }
+    }
+}
