45bfed1b677d2c38a4f0eca9666621d25e7c054a
Moinul Islam <moinulmithu@hotmail.com>
2016-01-01 07:58:05 +0600

    Silly task done

--- TaskManagement.Web/scripts/tasklist.js
+++ TaskManagement.Web/scripts/tasklist.js
@@ -1,13 +1,13 @@
 ﻿
 angular.module('tasklist', ['ngRoute', 'ngResource'])
     .config(['$routeProvider', function ($routeProvider) {
-        $routeProvider.when('/', { templateUrl: 'views/dashboard/dashboard.tpl.html', controller: 'dashboardController' })
-        .when('/dashboard', { templateUrl: 'views/dashboard/dashboard.tpl.html', controller: 'dashboardController' })
-        .when('/projects', { templateUrl: 'views/projects/project.tpl.html', controller: 'projectcontroller' })
-        .when('/projectdetail/:id', { templateUrl: 'views/projects/projectdetail.tpl.html', controller: 'projectdetailcontroller' })
-        
-        .when('/tasks', { templateUrl: 'views/tasks/task.tpl.html', controller: 'taskController' })
-        .when('/taskdetail/:id', { templateUrl: 'views/tasks/taskDetail.tpl.html', controller: 'taskdetailController' })
+          $routeProvider.when('/', { templateUrl: 'views/dashboard/dashboard.tpl.html', controller: 'dashboardController' })
+                        .when('/dashboard', { templateUrl: 'views/dashboard/dashboard.tpl.html', controller: 'dashboardController' })
+                        .when('/projects', { templateUrl: 'views/projects/project.tpl.html', controller: 'projectcontroller' })
+                        .when('/createProject', { templateUrl: 'views/projects/createProject.tpl.html', controller: 'createProjectcontroller' })
+                        .when('/projectdetail/:id', { templateUrl: 'views/projects/projectdetail.tpl.html', controller: 'projectdetailcontroller' })
+                        .when('/tasks', { templateUrl: 'views/tasks/task.tpl.html', controller: 'taskController' })
+                        .when('/taskdetail/:id', { templateUrl: 'views/tasks/taskDetail.tpl.html', controller: 'taskdetailController' })
          .otherwise({ redirecTo: '/' });
     }]);
     
